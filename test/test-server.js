d15aa8df60eb09e694e25e47499025062504d22c
Josh Kim <josh94kim@berkeley.edu>
2017-09-06 18:11:27 -0700

    integration testing for /recipes

--- test/test-server.js
+++ test/test-server.js
@@ -142,3 +142,82 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('Recipes', function() {
+  before(function() {
+    return runServer();
+  });
+  after(function() {
+    return closeServer();
+  });
+
+  it('Should list items on GET', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.have.length.of.at.least(1);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  it('Should add an item on POST', function() {
+      const newItem = {
+        name: 'grilled cheese',
+        ingredients: ['bread', 'cheese']
+      };
+      return chai.request(app)
+        .post('/recipes')
+        .send(newItem)
+        .then(function(res) {
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.include.keys('id', 'name', 'ingredients');
+            res.body.name.should.equal(newItem.name);
+            res.body.ingredients.should.be.a('array');
+            res.body.ingredients.should.include.members(newItem.ingredients);
+        });
+  });
+
+  it('Should update items on PUT', function() {
+    const updateData = {
+      name: 'grilled cheeseburger',
+      ingredients: ['buns', 'cheese', 'ground beef']
+    };
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.include.keys('id', 'name', 'ingredients')
+      res.body.name.should.equal(updateData.name);
+      res.body.id.should.equal(updateData.id);
+      res.body.ingredients.should.include.members(updateData.ingredients);
+    });
+  });
+
+  it('Should delete items on DELETE', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+        .delete(`/recipes/${res.body[0].id}`);
+    })
+    .then(function(res){
+      res.should.have.status(204);
+    });
+  });
+});
